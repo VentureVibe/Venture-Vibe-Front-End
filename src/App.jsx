8a671466c0dc7bde075cb7d8327ba071834ac304
Rashmika Guruge <rashmikaguruge01@gmail.com>
2024-06-12 11:52:48 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+import TravelPlan from './pages/travelPlan/TravelPlan';
+import Navbar from './components/navbar/Navbar';
+import { RouterProvider, createBrowserRouter, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,  // Use the Layout component for the root path
+      children: [
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/travelplan",
+          element: <TravelPlan />,
+        },
+      ],
+    },
+  ]);
 
   return (
-    <>
-      <h1>Hello vite</h1>
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/travelPlan/TravelPlan.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TravelPlan = () => {
+  return (
+    <div>
+        <h2>Travel plan</h2>
+    </div>
+  )
+}
+
+export default TravelPlan
