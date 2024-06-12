c279ba4ad328eb1187f2a79f28bc7ca5d3a7e0b2
AkilaBuwaneka <akilamg2000@gmail.com>
2024-06-12 15:27:03 +0530

    added to cervice provider

--- src/pages/serviceProvider/ServiceProviderUser.jsx
+++ src/pages/serviceProvider/ServiceProviderUser.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './ServiceProviderUser.scss'
+import Navbar from '../../components/navbar/Navbar'
 
 const ServiceProviderUser = () => {
   return (
     <div>
-      <center className='sp'>Service Provider User Side</center>
+        <Navbar />
+      <center className='title'>Service Provider User Side</center>
+      <div className='page'>
+        <div className='left'></div>
+        <div className='right'></div>
+      </div>
     </div>
   )
 }
