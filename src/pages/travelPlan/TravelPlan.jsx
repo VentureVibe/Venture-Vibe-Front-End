b8a24b34cba437c28cf5241ca82ab3a4c9486737
Rashmika Guruge <rashmikaguruge01@gmail.com>
2024-06-12 14:11:48 +0530

    add tripplanning

---
+++ src/pages/travelPlan/TravelPlan.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./TravelPlan.scss"
+import NavbarTravelplan from '../../components/navbar_travelplan/NavbarTravelplan'
+
+const TravelPlan = () => {
+  return (
+    <>
+        <div className='trvelplan'>
+          <div className='container'>
+            <div className='planning'>
+              <NavbarTravelplan />
+              <div className='plan-container'>
+                <div className='plan-sidebar'></div>
+                <div className='plan-list'></div>
+              </div>
+            </div>
+            <div className='map'><h3>map</h3></div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default TravelPlan
