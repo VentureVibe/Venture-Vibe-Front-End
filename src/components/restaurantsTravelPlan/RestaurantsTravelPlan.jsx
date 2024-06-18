27bb86652894c212b811fdd03f92bbc8fb64046e
Rashmika Guruge <rashmikaguruge01@gmail.com>
2024-06-18 23:41:39 +0530

    add expenses

--- src/components/restaurantsTravelPlan/RestaurantsTravelPlan.jsx
+++ src/components/restaurantsTravelPlan/RestaurantsTravelPlan.jsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 import './RestaurantsTravelPlan.scss';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import DeleteIcon from '@mui/icons-material/Delete';
+import PlaceTravelPlan from '../placeTravelPlan/PlaceTravelPlan';
 
 const RestaurantsTravelPlan = () => {
     const [isBottomContainerVisible, setIsBottomContainerVisible] = useState(false);
@@ -25,29 +23,9 @@ const RestaurantsTravelPlan = () => {
                 <i><KeyboardArrowDownIcon sx={{ color: '#747474' }}/></i>
                 <h2>Restaurants</h2>
             </div>
-            {isBottomContainerVisible && (<div className='show-restaurant-container'>
-                <div className="restaurant-name">
-                    <div className="icon-tag">
-                        <i><LocationOnIcon sx={{ color: '#F68712', fontSize: 35 }}/></i>
-                        <span>1</span>   
-                    </div>
-                    <span>Hungry Restaurant Galle</span>
-                    <i className='delete'><DeleteIcon sx={{ color: '#747474', fontSize: 20 }}/></i>
-                </div>
-                <div className="add-notes">
-                    <input type="text" placeholder='Add notes, links, etc. here'/>
-                </div>
-                <div className="bottom-show-restaurant">
-                    <div className="add-attachment">
-                        <i><AttachFileIcon sx={{ color: '#414143', fontSize: 18 }}/></i>
-                        <span>Attach</span>
-                    </div>
-                    <div className="add-cost">
-                        <i><AttachMoneyIcon sx={{ color: '#414143', fontSize: 18 }}/></i>
-                        <span>Add cost</span>
-                    </div>
-                </div>
-            </div>)}
+            {isBottomContainerVisible && (
+            <PlaceTravelPlan number='1' name='Hungry Restaurant Galle' color="#F68712"/>
+        )}
             {isBottomContainerVisible && (<div className='add-restaurant-container'>
                 <i><LocationOnIcon sx={{ color: '#414143', fontSize: 25 }}/></i>
                 <input type='text' placeholder='Add a restaurant'></input>
--- src/components/transportTravelPlan/TransportTravelPlan.jsx
+++ src/components/transportTravelPlan/TransportTravelPlan.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
 import './TransportTravelPlan.scss';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import DeleteIcon from '@mui/icons-material/Delete';
+import PlaceTravelPlan from '../placeTravelPlan/PlaceTravelPlan';
 
 const TransportTravelPlan = () => {
     const [isBottomContainerVisible, setIsBottomContainerVisible] = useState(false);
@@ -24,33 +21,9 @@ const TransportTravelPlan = () => {
                 <i><KeyboardArrowDownIcon sx={{ color: '#747474' }}/></i>
                 <h2>Transport Services</h2>
             </div>
-            {isBottomContainerVisible && (<div className='show-transport-container'>
-                <div className="transport-name">
-                    <div className="icon-tag">
-                        <i><LocationOnIcon sx={{ color: '#1BBC9B', fontSize: 35 }}/></i>
-                        <span>1</span>   
-                    </div>
-                    <span>Amas Transport</span>
-                    <i className='delete'><DeleteIcon sx={{ color: '#747474', fontSize: 20 }}/></i>
-                </div>
-                <div className="add-notes">
-                    <input type="text" placeholder='Add notes, links, etc. here'/>
-                </div>
-                <div className="bottom-show-transport">
-                    <div className="add-attachment">
-                        <i><AttachFileIcon sx={{ color: '#414143', fontSize: 18 }}/></i>
-                        <span>Attach</span>
-                    </div>
-                    <div className="add-cost">
-                        <i><AttachMoneyIcon sx={{ color: '#414143', fontSize: 18 }}/></i>
-                        <span>Add cost</span>
-                    </div>
-                </div>
-            </div>)}
-            {/* <div className='add-place-container'>
-                <i><LocationOnIcon sx={{ color: '#414143', fontSize: 25 }}/></i>
-                <input type='text' placeholder='Add a restaurant'></input>
-            </div> */}
+            {isBottomContainerVisible && (
+                <PlaceTravelPlan number='1' name='Amas Transport' color='#1BBC9B'/>
+            )}
            {isBottomContainerVisible && ( <div className='recommended-transport-heading-container' onClick={togglePlace}>
                 <i><KeyboardArrowDownIcon sx={{ color: '#747474' }}/></i>
                 <span>Add Transport Services</span>
