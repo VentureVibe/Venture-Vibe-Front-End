4880877b39771d4b6745e8d9027677949a8ec067
Kaveesha Weerakoon <savindukaveesha11@gmail.com>
2024-06-19 17:46:44 +0530

    Push

--- src/components/communityFeedLeft/CommunityFeedLeft.jsx
+++ src/components/communityFeedLeft/CommunityFeedLeft.jsx
@@ -1,11 +1,64 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './CommunityFeedLeft.scss'
 
 const CommunityFeedLeft = () => {
   return (
     <div className='CommunityFeedLeft'>
-      
+                 <Link to="/community">
+                  <div className="cont">
+                       <i class="fa-solid fa-square-rss"></i>
+                      <p>Feed</p>
+                  </div>   
+                  </Link>
+                  <div className="cont">
+                      <i class="fa-solid fa-users"></i>
+                      <p>Friends</p>
+                  </div>
+                  <div className="cont">
+                        <i class="fa-solid fa-square-caret-down"></i>              
+                        <p>Requests</p>
+                  </div>
+                  <div className="cont">
+                       <i class="fa-solid fa-square-check"></i>    
+                       <p>My Plannings</p>
+                  </div>
+               
+                  <div className="cont">
+                        <i class="fa-solid fa-hotel"></i>                      
+                        <p>Accomodations</p>
+                  </div>
+                  <div className="cont">
+                        <i class="fa-solid fa-person-hiking"></i>            
+                        <p>Event & Activities</p>
+                  </div>
+                  <div className="cont">
+                        <i class="fa-solid fa-car-side"></i>             
+                        <p>Transpotation</p>
+                  </div>
+                  <Link to="/travelplan">
+                  <div className="cont">
+                        <i class="fa-solid fa-person-snowboarding"></i>              
+                        <p>Plan Now</p>
+                  </div>
+                  </Link> 
+                  <div className="publish">
+                    <i class="fa-solid fa-upload"></i>
+                    <h4>Post Now</h4>
+                  </div>
     </div>
   )
+}
+
+const cont =({Icon,name,link})=>{
+    return(
+        <Link to={link}>
+             <div className="cont">
+                 <i class={Icon}></i>
+                 <p>{name}</p>
+            </div> 
+        </Link> 
+    );
 }
 
 export default CommunityFeedLeft
--- src/pages/community/Community.jsx
+++ src/pages/community/Community.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import "./Community.scss"
 import NavbarCommunity from '../../components/navbarCommunity/NavbarCommunity';
 import CommunityFriends from '../../components/communityFriends/CommunityFriends';
-import CommunityFeed from '../../components/communityFeed/CommunityFeed';
 import { Link, Outlet } from 'react-router-dom';
-
+import CommunityFeedLeft from '../../components/communityFeedLeft/CommunityFeedLeft';
 
 const Community = () => {
   return (
@@ -12,52 +11,9 @@ const Community = () => {
       <div className='container'>
            <NavbarCommunity/>
            <div className="bottom">
-               <div className="bottom-one">
-
-                  <Link to="/community">
-                  <div className="cont">
-                       <i class="fa-solid fa-square-rss"></i>
-                      <p>Feed</p>
-                  </div>   
-                  </Link> 
-                  <div className="cont">
-                      <i class="fa-solid fa-users"></i>
-                      <p>Friends</p>
-                  </div>
-                  <div className="cont">
-                        <i class="fa-solid fa-square-caret-down"></i>              
-                        <p>Requests</p>
-                  </div>
-                  <div className="cont">
-                       <i class="fa-solid fa-square-check"></i>    
-                       <p>My Plannings</p>
-                  </div>
-               
-                  <div className="cont">
-                        <i class="fa-solid fa-hotel"></i>                      
-                        <p>Accomodations</p>
-                  </div>
-                  <div className="cont">
-                        <i class="fa-solid fa-person-hiking"></i>            
-                        <p>Event & Activities</p>
-                  </div>
-                  <div className="cont">
-                        <i class="fa-solid fa-car-side"></i>             
-                        <p>Transpotation</p>
-                  </div>
-                  <Link to="/travelplan">
-                  <div className="cont">
-                        <i class="fa-solid fa-person-snowboarding"></i>              
-                        <p>Plan Now</p>
-                  </div>
-                  </Link> 
-                  <div className="publish">
-                    <i class="fa-solid fa-upload"></i>
-                    <h4>Post Now</h4>
-                  </div>
-                </div>
+                <CommunityFeedLeft/>
                 <Outlet></Outlet>
-               <CommunityFriends/>
+                <CommunityFriends/>
 
            </div>
       </div>
