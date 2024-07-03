7c3886341673e8b3ffd94117663f3304ea78781e
Rashmika Guruge <rashmikaguruge01@gmail.com>
2024-07-03 22:45:34 +0530

    update

--- src/components/navbarCommunity/NavbarCommunity.jsx
+++ src/components/navbarCommunity/NavbarCommunity.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import "./NavbarCommunity.scss"
-import logo from "../../assets/3.png"
-import man from "../../assets/man.jpg"
-
+import React, { useState } from 'react';
+import './NavbarCommunity.scss';
+import logo from '../../assets/3.png';
+import man from '../../assets/man.jpg';
+import { useNavigate } from 'react-router-dom';
 
 const NavbarCommunity = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate(); // Access navigate function for navigation
+
+  const handleSearch = () => {
+    if (searchTerm.trim() !== '') {
+      navigate(`/community/search/post/${encodeURIComponent(searchTerm)}`);
+    }
+  };
+
   return (
     <div className='navbar'>
-        <div className='logo'>
-          <img src={logo} alt='logo'/>
-          <h2 className='logo-name'>Venture Vibe</h2>
-        </div>
-        <div className='nav-search'>
-           <input type="text" placeholder='Search'/>
-           <i class="fa-solid fa-magnifying-glass"></i>
-        </div>
-        <div className="profile-bar">
-           <i class="fa-regular fa-bell"></i>
-           <h4>Kaveesha</h4>
-          <img src={man} alt="" />
-        </div>
+      <div className='logo'>
+        <img src={logo} alt='logo'/>
+        <h2 className='logo-name'>Venture Vibe</h2>
+      </div>
+      <div className='nav-search'>
+        <input
+          type="text"
+          placeholder='Search'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              handleSearch();
+            }
+          }}
+        />
+        <i className="fa-solid fa-magnifying-glass" onClick={handleSearch}></i>
+      </div>
+      <div className="profile-bar">
+        <i className="fa-regular fa-bell"></i>
+        <h4>Kaveesha</h4>
+        <img src={man} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarCommunity
+export default NavbarCommunity;
