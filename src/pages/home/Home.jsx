e4055a413375f9946d1411dee46be9d424179d78
Kaveesha Weerakoon <94215796+Kaveesha-Weerakoon@users.noreply.github.com>
2024-07-03 18:06:16 +0530

    Revert "Trip planning"

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Nav from '../../components/homepage/Nav'
+import DestinationDiv from '../../components/homepage/DestinationDiv';
+import StoryDiv from '../../components/homepage/StoryDiv';
+import MilestonesDiv from '../../components/homepage/MilestonesDiv';
+import SubscribeDiv from '../../components/homepage/SubscribeDiv';
+import Main from '../../components/homepage/Main';
 
 const Home = () => {
   return (
-    <h2>Home</h2>
+    <>
+    {/*<Nav />*/}
+    <Main />
+    <DestinationDiv />
+    <StoryDiv />
+    <MilestonesDiv />
+    <SubscribeDiv />
+    </>
   )
 }
 
