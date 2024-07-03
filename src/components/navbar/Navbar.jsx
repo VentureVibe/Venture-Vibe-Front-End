941a00fedd620c088801cc71be2b05e93eeb16a7
AkilaBuwaneka <akilamg2000@gmail.com>
2024-07-04 00:30:53 +0530

    added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div><h2>Navbar</h2></div>
+    <div className='navbar'>
+      <img src="/src/assets/3.png" />
+      <ul className="list">
+        <li>Community</li>
+        <li>Hotels</li>
+        <li>Events</li>
+        <li>Travel Guides</li>
+      </ul>
+      <ul className="button">
+        <li className="login"><button>Login</button></li>
+        <li className="signup"><button>SignUp</button></li>
+      </ul>
+    </div>
   )
 }
 
