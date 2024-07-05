4cb9c5fc7f73469dc7622bcb4bdd6ef8daa4ce7c
Randika BLS <sithumrandika85@gmail.com>
2024-07-05 15:07:54 +0530

    navbar

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../../components/admin/sidebar/Sidebar";
 import "./Admin.scss";
+import Navbar from "../components/navbar/Navbar";
 
 const Admin = () => {
   return (
     <>
+      <Navbar />
       <Sidebar />
     </>
   );
