fbdcb256ffc66a2cb48ce171f44fb4e6082e2443
Rashmika Guruge <rashmikaguruge01@gmail.com>
2024-07-24 22:14:49 +0530

    add community post

--- src/components/communityPost/CommunityPost.jsx
+++ src/components/communityPost/CommunityPost.jsx
@@ -4,20 +4,23 @@ import man from "../../assets/profilepics/Profile4.jpg";
 import ella from "../../assets/ella.jpg";
 import PopUpMain from '../popupmain/PopUpMain';
 import CommunityPostPop from '../communityPostPop/CommunityPostPop';
+import noAvatar from "../../assets/noavatar.jpg";
 
-const CommunityPost = ({ name, likes, comments, description, imageSrc, profile }) => {
+const CommunityPost = ({ name, likes, comments, description, imageSrc }) => {
   const [showPopup, setShowPopup] = useState(false); // Start with popup hidden
-  const [data, setData] = useState({ name, likes, comments, description, imageSrc, profile });
+  const [data, setData] = useState({ name, likes, comments, description, imageSrc });
 
   const toggleModal = () => {
     setShowPopup(!showPopup); // Toggle the state of showPopup
   };
 
+  console.log(imageSrc);
+
   return (
     <div className='CommunityPost' onClick={toggleModal}>
       <img src={imageSrc} alt="" />
       <div className="detail">
-        <img src={profile} alt="" />
+        <img src={noAvatar} alt="" />
         <h2>{name}</h2>
         <div className="like">
           <i className="fa-solid fa-heart"></i>
