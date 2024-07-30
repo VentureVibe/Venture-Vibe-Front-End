ff94ff17e1af2b4c3eb7058bc109a84ae5026561
AkilaBuwaneka <akilamg2000@gmail.com>
2024-07-30 15:29:10 +0530

    added

--- src/components/addEventListing/AddEventListing.jsx
+++ src/components/addEventListing/AddEventListing.jsx
@@ -1,111 +1,25 @@
-// import React, { useState } from 'react';
-// import './AddEventListing.scss';
-// import CloseIcon from '@mui/icons-material/Close';
-
-// function AddEventListing({ onClose}) {
-
-//   // Handle form input changes
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData(prevState => ({
-//       ...prevState,
-//       [name]: value,
-//     }));
-//   };
-
-//   // Handle form submission
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // Submit form data
-//     console.log('Form data submitted:', formData);
-//     // Reset form or redirect user after submission
-//   };
-
-//   return (
-//     <div className='add-event-listing-form'>
-//       <div className="closing-i" onClick={onClose}>
-//         <i><CloseIcon sx={{ color: '#747474', fontSize: 16 }} /></i>
-//       </div>
-//     <form onSubmit={handleSubmit}>
-//       <h2>Add Event Listing</h2>
-//       {/* Assuming you have a way to handle image uploads, omitted here for brevity */}
-//       <div>
-//         <label htmlFor="title">Title:</label>
-//         <input
-//           type="text"
-//           id="title"
-//           name="title"
-//           //value={formData.title}
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="location">Location:</label>
-//         <input
-//           type="text"
-//           id="location"
-//           name="location"
-//           //value={formData.location}
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="description">Description:</label>
-//         <textarea
-//           id="description"
-//           name="description"
-//           //value={formData.description}
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="price">Price:</label>
-//         <input
-//           type="number"
-//           id="price"
-//           name="price"
-//           //value={formData.price}
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="contactNumber">Contact Number:</label>
-//         <input
-//           type="text"
-//           id="contactNumber"
-//           name="contactNumber"
-//           //value={formData.contactNumber}
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="email">Email:</label>
-//         <input
-//           type="email"
-//           id="email"
-//           name="email"
-//           //value={formData.email}
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <button type="submit">Add Event</button>
-//     </form>
-//     </div>
-//   );
-// }
-
-// export default AddEventListing;
-
 import React, { useState, useEffect, useRef } from "react";
 import "./AddEventListing.scss";
 import CloseIcon from "@mui/icons-material/Close";
+import { createEvent } from "../../services/events/eventServices";
+import { GetCurrentUserC } from "../../services/user/GetCurrentUserC";
+import Loading from "../loading/Loading";
 
 function AddEventListing({ onClose }) {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    price: "",
+    contactNumber: "",
+    email: "",
+    location: "",
+  });
   const [coordinates, setCoordinates] = useState({ lat: "", lng: "" });
   const mapRef = useRef(null);
   const autocompleteRef = useRef(null);
   const markerRef = useRef(null);
+  const [imagePreview, setImagePreview] = useState("");
+  const [loading, setLoading] = useState(false);
 
   // Initialize Google Maps Places Autocomplete
   useEffect(() => {
@@ -190,24 +104,76 @@ function AddEventListing({ onClose }) {
     }));
   };
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const previewUrl = URL.createObjectURL(file);
+      setImagePreview(previewUrl);
+      setFormData({
+        ...formData,
+        eventImage: file,
+      });
+    }
+  };
+
   // Handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const submittedData = {
-      ...formData,
-      coordinates,
-    };
-    console.log(
-      "Form data submitted:",
-      submittedData.title,
-      submittedData.coordinates,
-      submittedData.email
-    );
-    // Reset form or redirect user after submission
+
+    try {
+      const decodedToken = GetCurrentUserC();
+      const id = decodedToken.sub;
+
+      // Prepare event data
+      const submittedData = {
+        userId: id,
+        eventTitle: formData.title,
+        eventDescription: formData.description,
+        eventPrice: parseFloat(formData.price),
+        eventLat: coordinates.lat,
+        eventLon: coordinates.lng,
+        contactEmail: formData.email,
+        contactPhone: formData.contactNumber,
+      };
+
+      // Create FormData object
+      const formDataObj = new FormData();
+      formDataObj.append("event", JSON.stringify(submittedData));
+      if (formData.eventImage) {
+        formDataObj.append("image", formData.eventImage);
+      }
+      setLoading(true);
+      // Call the API
+      await handleCreateEvent(formDataObj);
+
+      // Reset form or redirect user after submission
+      setFormData({
+        title: "",
+        description: "",
+        price: "",
+        contactNumber: "",
+        email: "",
+      });
+      setCoordinates({ lat: null, lon: null });
+      if (onClose) onClose();
+      setLoading(false);
+      window.location.reload();
+    } catch (error) {
+      console.error("Error creating event:", error);
+    }
+  };
+
+  const handleCreateEvent = async (formDataObj) => {
+    try {
+      const createdEvent = await createEvent(formDataObj);
+    } catch (error) {
+      console.error("Failed to create event:", error);
+    }
   };
 
   return (
     <div className="add-event-listing-form">
+      {loading && <Loading />}
       <div className="head-add-event">
         <h2>Add Event Listing</h2>
         <i onClick={onClose}>
@@ -260,6 +226,25 @@ function AddEventListing({ onClose }) {
               onChange={handleChange}
             />
           </div>
+          <div>
+            <label htmlFor="eventImage">Event Image</label>
+            <input
+              type="file"
+              id="eventImage"
+              name="eventImage"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+          </div>
+          {imagePreview && (
+            <div>
+              <img
+                src={imagePreview}
+                alt="Preview"
+                style={{ width: "100px", height: "100px", marginTop: "10px" }}
+              />
+            </div>
+          )}
           <button type="submit">Add Event</button>
         </div>
         <div className="add-event-right">
--- src/services/user/GetUser.jsx
+++ src/services/user/GetUser.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { jwtDecode } from "jwt-decode"; // Ensure you use the correct import
+import { jwtDecode } from "jwt-decode";
 import { GetCurrentUserC } from "./GetCurrentUserC";
 
 export const GetUser = async () => {
@@ -9,22 +9,16 @@ export const GetUser = async () => {
       throw new Error("No token found");
     }
 
-    //const decodedToken = jwtDecode(token);
     const decodedToken = GetCurrentUserC();
     const email = decodedToken.email;
     const id = decodedToken.sub;
 
-    // Make a GET request to fetch user details by email
     const response = await axios.get(
-      `http://localhost:8080/api/v1/public/traveler/${id}` /*, {
-      params: { email } // Send email as a query parameter
-    }*/
+      `http://localhost:8080/api/v1/public/traveler/${id}`
     );
-
-    // Return the user response
     return response.data;
   } catch (error) {
     console.error("Error fetching user:", error.message);
-    throw error; // Rethrow the error to be handled by the calling function
+    throw error;
   }
 };
