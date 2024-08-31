ba2290858f12e5249ced3f16877be27aa8ea0fcb
Akila Buwaneka <136682897+AkilaBuwaneka@users.noreply.github.com>
2024-08-31 12:06:27 +0530

    Update cognitoConfig.jsx

--- src/cognitoConfig.jsx
+++ src/cognitoConfig.jsx
@@ -1,6 +1,4 @@
 export const poolData = {
-    // UserPoolId: 'eu-north-1_iEkEv3B6q',
-    // ClientId: '7gejr9ke18o65cbhbe2au76ff7',
-    UserPoolId: 'eu-north-1_sUnFwzIil',
-    ClientId: '1ffq0p2st2vs1l9a4p2ga20gd5',
-  };
+  UserPoolId: import.meta.env.VITE_USERPOOL_ID,
+  ClientId: import.meta.env.VITE_CLIENT_ID,
+};
