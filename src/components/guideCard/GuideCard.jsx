1ae397d17859d121adc49ea6edcc4631d237d96c
AkilaBuwaneka <akilamg2000@gmail.com>
2024-11-20 21:43:12 +0530

    updated

--- src/components/guideCard/GuideCard.jsx
+++ src/components/guideCard/GuideCard.jsx
@@ -1,28 +1,44 @@
-import React from 'react';
-import './GuideCard.scss';
-import { Link } from 'react-router-dom';
-import StarRateIcon from '@mui/icons-material/StarRate';
+import React from "react";
+import "./GuideCard.scss";
+import { Link } from "react-router-dom";
+import StarRateIcon from "@mui/icons-material/StarRate";
 
-const GuideCard = ({ img, name, price, id, location, contactNumber, rating}) => {
+const GuideCard = ({
+  img,
+  name,
+  price,
+  id,
+  location,
+  contactNumber,
+  rating,
+}) => {
   return (
-    <div className='guide'>
+    <div className="guide">
       <Link to={`/guideprofile`}>
         <div className="body-container">
-            <img src={img} alt={name} />
-            <span className='title'>{name}</span>
-            <div className='rating'>
-                <i><StarRateIcon sx={{ color: '#414143', fontSize: 15 }}/></i>
-                <i><StarRateIcon sx={{ color: '#414143', fontSize: 15 }}/></i>
-                <i><StarRateIcon sx={{ color: '#414143', fontSize: 15 }}/></i>
-                <i><StarRateIcon sx={{ color: '#414143', fontSize: 15 }}/></i>
-            </div>
-            <span className='location'>Location : {location}</span>
-            <span className='price'>{price}$</span>
-            <span className='contactNumber'>{contactNumber}</span>
+          <img src={img} alt={name} />
+          <span className="title">{name}</span>
+          <div className="rating">
+            <i>
+              <StarRateIcon sx={{ color: "#414143", fontSize: 15 }} />
+            </i>
+            <i>
+              <StarRateIcon sx={{ color: "#414143", fontSize: 15 }} />
+            </i>
+            <i>
+              <StarRateIcon sx={{ color: "#414143", fontSize: 15 }} />
+            </i>
+            <i>
+              <StarRateIcon sx={{ color: "#414143", fontSize: 15 }} />
+            </i>
+          </div>
+          <span className="location">Location : {location}</span>
+          <span className="price">{price}$</span>
+          <span className="contactNumber">{contactNumber}</span>
         </div>
       </Link>
     </div>
   );
-}
+};
 
 export default GuideCard;
